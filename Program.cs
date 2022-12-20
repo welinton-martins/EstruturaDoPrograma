37e586d72bbe8da3681ad5beeb85c7a1cf9b1436
Welinton Martins <57647104+welinton-martins@users.noreply.github.com>
2022-12-20 12:00:55 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EstruturaDoPrograma;
+using Exemplos;
+
+class Program
+{
+    static void Main()
+    {
+        var s = new Pilha();
+        s.Empilha(1);
+        s.Empilha(10);
+        s.Empilha(100);
+        Console.WriteLine(s.Desempilha());
+        Console.WriteLine(s.Desempilha());
+        Console.WriteLine(s.Desempilha());
+        Console.WriteLine(s.Desempilha());
+    }
+}
